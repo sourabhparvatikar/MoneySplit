1180296f151c14a0b3a815792ecae26f24d52e66
gurupad93 <gurupad93@gmail.com>
2020-05-02 19:33:32 -0400

    flake8 things

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 
 from backend import config # noqa
+from backend.api_resources import add_resource
 
 
 app = Flask(__name__)
@@ -14,5 +15,4 @@ migrate = Migrate(app, db)
 
 
 from backend import views, models # noqa
-from backend.api_resources import add_resource
 add_resource(api)
--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,7 @@
-from backend import app, api
 from flask_restful import Resource
+
 
 class HelloWorld(Resource):
 
     def get(self):
         return "Hello, World!"
-
